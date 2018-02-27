54b76486d0a16a49a39dd3b6c34c60590c192c2e
Julien Leicher <jleicher@gmail.com>
2018-02-27 15:35:32 +0100

    Edited checkbox

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="checkbox">
-    <input type="checkbox" :checked="value" @change="$emit('change', !value)" />
+  <div :class="{ 'checkbox': true, 'checkbox--checked': value }">
+    <input type="checkbox" class="checkbox__input" :checked="value" @change="$emit('change', !value)" />
   </div>
 </template>
 
@@ -17,7 +17,28 @@ export default {
 @import "./../_vars.scss";
 
 .checkbox {
+  &::after {
+    content: '';
+    border: 1px solid color(brand);
+    border-radius: 5px;
+    color: black;
+    display: inline-block;
+    height: 16px;
+    width: 16px;
+    transition: all 0.2s;
+  }
 
+  &--checked {
+    &::after {
+      background-color: color(brand);
+      transform: rotate(90deg);
+    }
+
+  }
+
+  &__input {
+    visibility: hidden;
+  }
 }
 </style>
 
